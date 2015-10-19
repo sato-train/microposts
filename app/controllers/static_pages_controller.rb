179c8f722efe344aa30865c469fc356e66b9fd03
sato <post.sato+train@gmail.com>
2015-10-19 06:24:03 +0000

    add home view's timeline

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
-    #current_user.microposts.buildは、Micropost.new(user_id: current_user.id)と同じです。
-    #前者の方はcurrent_userのhas_many :micropostsで生成されるbuildメソッドを使用していて、
-    #確実にuser_idが紐付いたデータを作成できるのでこちらを使用するようにしましょう。
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      #current_user.microposts.buildは、Micropost.new(user_id: current_user.id)と同じです。
+      #前者の方はcurrent_userのhas_many :micropostsで生成されるbuildメソッドを使用していて、
+      #確実にuser_idが紐付いたデータを作成できるのでこちらを使用するようにしましょう。
+      @micropost = current_user.microposts.build
+      
+      #feed_itemsで現在のユーザーのフォローしているユーザーを取得し、
+      #order(created_at: :desc)投稿日で並び替えを行っています
+      #includes(:user)の部分は、つぶやきに含まれるユーザー情報をあらかじめ先読み
+      #（プリロード）する処理を行うために用います
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
   end
 end
