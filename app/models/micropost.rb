e9404e8cb79ae37e1bf3c9ee0d60e14f72fcfcce
sato <post.sato+train@gmail.com>
2015-10-22 01:48:08 +0000

    set order def

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,6 @@ class Micropost < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140}
+
+  default_scope { order('created_at DESC') }
 end
